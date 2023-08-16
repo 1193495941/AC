23e4d41481d6343382a8a890462ef52f83f45d2b
阳超逸 <1193495941@qq.com>
2023-08-16 17:02:22 +0800

    Initial commit

--- DagTask/env/my_env.py
+++ DagTask/env/my_env.py
@@ -69,55 +69,65 @@ class env():
         # 获取节点数量
         self.num_nodes = len(adjacency_matrix)
         # 追踪节点被调度情况（是否全部调度完成）
-        self.nodes_done = np.zeros(self.num_nodes, dtype=np.bool)
+        self.nodes_done = np.zeros(self.num_nodes, dtype=np.bool_)
         # 环境重置
         self.reset()
+        self.get_action_space()
 
     def reset(self):  # 环境重置
-        # 当前节点
-        self.current_node = 0
         # 已完成节点数量
-        self.nodes_done = 0
+        self.nodes_done = np.zeros(self.num_nodes, dtype=np.bool_)
         # 是否完成
         self.is_done = False
-        return self.get_state_vec()
+        return self.get_state()
 
     '''
         获取节点state的tensor
         '''
 
-    def get_state_vec(adj_matrix):
-        state_vec = torch.tensor(adj_matrix, dtype=torch.float32).view(-1)
-        return state_vec
+    def get_state(self):
+        return torch.tensor(self.nodes_done, dtype=torch.float32)
 
     '''
         获取动作空间
         '''
 
-    def get_action_space(cur_adj_matrix, task_index):
-        # task_index代表当前调度节点
-        num_task = cur_adj_matrix.shape[0]
-        # 生成调度候选集
-        task_list = []
-        for i in range(num_task):
-            if sum(cur_adj_matrix[i, :]) != 0 or sum(cur_adj_matrix[:, i]) != 0:
-                task_list.append(i)
-        task_list.remove(task_index)
-        print(task_list)  # [1, 2, 3]
-        cur_adj_matrix[task_index, :] = 0
-        action_space = []
-        for j in task_list:
-            if np.sum(cur_adj_matrix[:, j]) == 0:
-                action_space.append(j)
-        cur_adj_matrix[task_index, :] = 0
-        return action_space, cur_adj_matrix
+    def get_action_space(self):
+        in_degrees = np.sum(self.adjacency_matrix, axis=0)
+        out_degrees = np.sum(self.adjacency_matrix, axis=1)
+        zero_in_degrees_nodes = np.where(in_degrees == 0)[0]
+        zero_in_and_out_degrees_nodes = np.where((in_degrees == 0) & (out_degrees == 0))[0]
+        action_space = list(set(zero_in_degrees_nodes) - set(zero_in_and_out_degrees_nodes))
+        # 找到只有对角线为1的节点
+        diagonal_nodes = np.where(np.diag(self.adjacency_matrix) == 1)[0]  # 对角线为1的节点
+        only_diagonal_nodes = []
+        for node in diagonal_nodes:
+            if (sum(self.adjacency_matrix[node, :]) + sum(self.adjacency_matrix[:, node]) == 2):
+                only_diagonal_nodes.append(node)
+        action_space.extend(only_diagonal_nodes)
+        action_space.sort()
 
-    def is_done(adj_matrix, done):
-        if sum(adj_matrix[:, :]) == 0:
-            done = True
-        else:
-            done = False
-        return done
+        if not action_space:
+            action_space = [self.num_nodes - 1]
+        return action_space
+
+    # def get_action_space(cur_adj_matrix, task_index):
+    #     # task_index代表当前调度节点
+    #     num_task = cur_adj_matrix.shape[0]
+    #     # 生成调度候选集
+    #     task_list = []
+    #     for i in range(num_task):
+    #         if sum(cur_adj_matrix[i, :]) != 0 or sum(cur_adj_matrix[:, i]) != 0:
+    #             task_list.append(i)
+    #     task_list.remove(task_index)
+    #     print(task_list)  # [1, 2, 3]
+    #     cur_adj_matrix[task_index, :] = 0
+    #     action_space = []
+    #     for j in task_list:
+    #         if np.sum(cur_adj_matrix[:, j]) == 0:
+    #             action_space.append(j)
+    #     cur_adj_matrix[task_index, :] = 0
+    #     return action_space
 
     def step(self, action):
         reward = 0
@@ -125,26 +135,17 @@ class env():
 
         if not self.nodes_done[action]:
             self.nodes_done[action] = True
+            # 将已调度节点的行置零
+            self.adjacency_matrix[action, :] = 0
+            cur_adj = self.adjacency_matrix
+            # print(self.adjacency_matrix)
             if np.all(self.nodes_done):
                 reward = 1
                 done = True
         else:
             print('out of action_space')
-        next_state = self.get_state_vec()
+        next_state = self.get_state()
 
         return next_state, reward, done
 
 
-# 假设有5个节点
-num_nodes = 5
-# 创建布尔数组，表示节点的调度状态
-nodes_done = np.zeros(num_nodes, dtype=np.bool_)
-# 假设节点2和节点4已经被调度完成
-nodes_done[2] = True
-nodes_done[4] = True
-if not nodes_done[1]:
-    # 输出节点的调度状态
-    print("Nodes done:", nodes_done)
-    print("nodes_done[2]:", nodes_done[1])
-else:
-    print("nodes_done[2]:",nodes_done[1])
--- DagTask/main.py
+++ DagTask/main.py
@@ -13,6 +13,53 @@ import torch.nn.functional as F
 import numpy as np
 from tqdm import tqdm
 from env.my_env import env
+import pygraphviz as pgv
+import re
+
+# 读取 .gv 文件并解析为图形对象
+file_path = "./env/data/meta_offloading_20/offload_random20_1/random.20.0.gv"
+with open(file_path, "r") as f:
+    gv_content = f.read()
+
+# 提取节点和边的信息
+node_pattern = r"(\d+) \[.*\]"
+edge_pattern = r"(\d+) -> (\d+) .*"
+
+nodes = re.findall(node_pattern, gv_content)
+edges = re.findall(edge_pattern, gv_content)
+
+# 确定节点的数量
+num_nodes = max([int(node) for node in nodes])
+
+# 初始化邻接矩阵
+gen_adjacency_matrix = np.zeros((num_nodes, num_nodes), dtype=int)
+# 填充邻接矩阵
+for src, dst in edges:
+    src_idx = int(src) - 1
+    dst_idx = int(dst) - 1
+    gen_adjacency_matrix[src_idx, dst_idx] = 1
+
+
+def zero_out(gen_adjacency_matrix):
+    # in_degrees = np.sum(gen_adjacency_matrix, axis=0)
+    out_degrees = np.sum(gen_adjacency_matrix, axis=1)
+    zero_out_degrees_nodes = np.where(out_degrees == 0)[0]
+    return zero_out_degrees_nodes
+
+
+# print(zero_in_and_out(gen_adjacency_matrix))
+zero_out_nodes = zero_out(gen_adjacency_matrix)
+gen_adjacency_matrix[zero_out_nodes, zero_out_nodes] = 1
+init_adjacency_matrix = gen_adjacency_matrix
+# print(init_adjacency_matrix)
+# 提取节点信息
+node_pattern = r"(\d+) \[size=\"(\d+)\",.*\]"
+nodes = re.findall(node_pattern, gv_content)
+
+# 将提取的节点信息转换为字典，其中键为节点索引，值为节点的size（作为时延）
+node_delay_map = {int(node[0]): int(node[1]) for node in nodes}
+
+# print("节点时延映射:", node_delay_map)
 
 '''
     定义GRU模型作为ActorCritic的输入
@@ -23,15 +70,17 @@ from env.my_env import env
 
 
 class Actor(nn.Module):
-    def __init__(self, state_dim, hidden_dim, output_dim):
+    def __init__(self, state_dim, hidden_dim, action_dim):
         super(Actor, self).__init__()
         self.gru = nn.GRU(state_dim, hidden_dim)
-        # GRU output
-        self.output = nn.Linear(hidden_dim, output_dim)
+        self.output = nn.Linear(hidden_dim, action_dim)
 
     def forward(self, x, hidden):
         output, hidden = self.gru(x.unsqueeze(0), hidden)
-        action_probs = torch.softmax(self.output(output), dim=-1)
+        action_logits = self.output(output)
+
+        action_probs = torch.softmax(action_logits, dim=-1)
+
         return action_probs, hidden
 
 
@@ -74,151 +123,123 @@ class ActorCritic(nn.Module):
 
 
 '''
-    定义 如何计算优势值 和 累计奖励 
-    '''
-
-
-def calculate_advantages(rewards, values, gamma=0.99, lamda=0.95):
-    advantages = []
-    gae = 0  # 广义优势估计
-
-    for t in reversed(range(len(rewards))):
-        delta = rewards[t] + gamma * values[t + 1] - values[t]
-        gae = delta + gamma * lamda * gae
-        advantages.insert(0, gae)
-
-    return advantages
-
-
-def calculate_returns(rewards, gamma=0.99):
-    returns = []
-    G = 0  # 累积折扣奖励
-
-    for t in reversed(range(len(rewards))):
-        G = rewards[t] + gamma * G
-        returns.insert(0, G)
-
-    return returns
-
-
-'''
-    计算Actor和Critic的损失
-    '''
-
-
-def calculate_losses(log_probs, advantages, values, returns):
-    # 策略梯度损失
-    actor_loss = -torch.stack(log_probs).sum() * torch.stack(advantages).mean()
-
-    # 均方误差损失
-    critic_loss = nn.MSELoss()(torch.stack(values), torch.stack(returns))
-
-    return actor_loss, critic_loss
-
-
-'''
     定义训练过程
     '''
 
 
 def train_actor_critic(env, model, num_epochs, learning_rate):
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-    # criterion = nn.MSELoss()
+    criterion = nn.NLLLoss()  # 负对数似然损失函数
+    critic_criterion = nn.MSELoss()
 
-    for i in range(10):
-        with tqdm(total=int(num_epochs / 10), desc='进度 %d' % i) as pbar:
-            for i_episode in range(int(num_epochs / 10)):
-                for epoch in range(num_epochs):
-                    state = env.reset()
-                    hidden = None
-                    log_probs = []
-                    values = []
-                    rewards = []
+    for epoch in range(num_epochs):
+        state = env.reset()
 
-                    while not env.is_done():
-                        state_tensor = torch.tensor(state, dtype=torch.float32).unsqueeze(0)
-                        action_probs, value, hidden = model(state_tensor, hidden)
-                        action_dist = torch.distributions.Categorical(action_probs)
-                        action = action_dist.sample()
+        hidden = None
+        log_probs = []
+        values = []
+        rewards = []
+        schedule = []
 
-                        next_state, reward, done = env.step(action.item())
+        while True:
+            # 获取可选动作
 
-                        log_prob = action_dist.log_prob(action)
-                        log_probs.append(log_prob)
-                        values.append(value)
-                        rewards.append(reward)
+            action_space = env.get_action_space()
+            action_space_size = env.num_nodes  # 动作空间长度
+            available_actions = torch.zeros(action_space_size)  # 初始化为0
+            available_actions[action_space] = 1.0  # 设置可选择的动作为1
 
-                        state = next_state
-                    # 计算Advantage和Returns
-                    advantages = calculate_advantages(rewards, values)
-                    returns = calculate_returns(rewards)
+            # 状态
+            state_tensor = state.unsqueeze(0)
+            # 模型输出 action_probs概率分布
+            action_probs, value, hidden = model(state_tensor, hidden)
 
-                    # 计算Actor和Critic的损失
-                    actor_loss, critic_loss = calculate_losses(log_probs, advantages, values, returns)
+            masked_action_probs = action_probs * available_actions  # 将不可选择的动作的概率置为0
+            action_probs_normalized = masked_action_probs / masked_action_probs.sum()  # 归一化概率
 
-                    # 梯度更新
-                    optimizer.zero_grad()
-                    loss = actor_loss + critic_loss
-                    loss.backward()
-                    optimizer.step()
-                pbar.update(1)
+            # action_dist = torch.distributions.Categorical(action_probs)
 
+            action = torch.argmax(action_probs_normalized).item()  # 选择概率最大的动作
+            schedule.append(action)
+            eps = 1e-8
+            log_prob = torch.log(action_probs_normalized[0] + eps)  # 使用选择的动作的对数概率作为训练目标
+            log_probs.append(log_prob)
+            values.append(value)
 
-'''
-    使用训练好的模型进行任务调度
-    '''
+            next_state, reward, done = env.step(action)
+            rewards.append(float(reward))
+
+            state = next_state
+            # print(state)
+            # print(schedule)
 
 
-def schedule_tasks(env, model):
-    state = env.reset()
-    hidden = None
-    schedule = []
+            if done:
+                # 重置adjacency_matrix为原始输入的邻接矩阵
+                # env.adjacency_matrix = np.array([[0, 1, 1, 0],
+                #                                  [0, 0, 0, 1],
+                #                                  [0, 0, 0, 1],
+                #                                  [0, 0, 0, 0]])
+                env.adjacency_matrix = np.copy(init_adjacency_matrix)
 
-    while not env.is_done():
-        state_tensor = torch.tensor(state, dtype=torch.float32).unsqueeze(0)
-        action_probs, _, hidden = model(state_tensor, hidden)
-        action = torch.argmax(action_probs).item()
+                print(f"done:{epoch + 1}/{num_epochs},Loss:{None},调度顺序为：{schedule} ")
+                break
 
-        next_state, _, _ = env.step(action)
-        schedule.append(action)
-
-        state = next_state
-
-    return schedule
+        # 梯度更新
+        actor_loss = -torch.stack(log_probs).sum()  # 使用负对数似然作为损失
+        critic_loss = critic_criterion(torch.tensor(rewards), torch.stack(values).squeeze())
+        loss = actor_loss + critic_loss
+        loss.backward()
+        optimizer.step()
 
 
 if __name__ == "__main__":
-    # 获取 初始 task 的 task_index 以及 adj_matrixer
-    #
-    adj_matrix = np.array([[0, 1, 1, 0, 0],
-                           [0, 0, 0, 1, 0],
-                           [0, 0, 0, 0, 1],
-                           [0, 0, 0, 0, 1],
-                           [0, 0, 0, 0, 0]
-                           ])
-    # 假设DAG是由节点和依赖关系构成的，依赖关系表示为一个邻接矩阵
-    # 例如，dependencies[i][j] 表示任务 i 依赖任务 j
-    dependencies = [[0, 1, 0, 0],
-                    [0, 0, 1, 0],
-                    [0, 0, 0, 1],
-                    [0, 0, 0, 0]]
-
-    # 状态维度、动作维度、隐藏状态维度
-    state_dim = adj_matrix.shape[0] * adj_matrix.shape[0]  # 5
-    print(state_dim)
-    action_dim = 1  # 1
-    hidden_dim = 128  # 128
-    print(env.get_state_vec(adj_matrix, 0))
-
     # 初始化参数
-    num_epochs = 100
+    num_epochs = 1000
     learning_rate = 0.001
-    model = ActorCritic(state_dim, hidden_dim, action_dim)
-    env = env
 
+    # 假设DAG是由节点和依赖关系构成的，依赖关系表示为一个邻接矩阵
+    # adjacency_matrix = np.array([[0, 1, 1, 0],
+    #                              [0, 0, 0, 1],
+    #                              [0, 0, 0, 1],
+    #                              [0, 0, 0, 0]])
+    adjacency_matrix = np.copy(init_adjacency_matrix)
+    # print(adjacency_matrix)
+
+    env = env(adjacency_matrix)
+    action_space = env.get_action_space()
+    model = ActorCritic(state_dim=env.num_nodes, hidden_dim=32, action_dim=env.num_nodes)
     # 训练Actor-Critic模型
     train_actor_critic(env, model, num_epochs, learning_rate)
 
     # 使用训练好的模型进行任务调度
-    schedule = schedule_tasks(env, model)
-    print("任务调度:", schedule)
+    state = env.reset()
+    hidden = None
+    schedule = []
+
+    # while True:
+    #     action_space = env.get_action_space()
+    #     state_tensor = state.unsqueeze(0)
+    #     action_probs, _, hidden = model(state_tensor, hidden)
+    #
+    #     available_actions = torch.tensor([action_space], dtype=torch.float32)
+    #     masked_action_probs = action_probs * available_actions
+    #
+    #     action = torch.argmax(masked_action_probs).item()
+    #
+    #     next_state, _, done = env.step(action)
+    #     schedule.append(action)
+    #
+    #     # 将已调度节点的行置零
+    #     adjacency_matrix[action, :] = 0
+    #     # 重置adjacency_matrix为原始输入的邻接矩阵
+    #     if done:
+    #         adjacency_matrix = np.array([[0, 1, 1, 0],
+    #                                      [0, 0, 0, 1],
+    #                                      [0, 0, 0, 1],
+    #                                      [0, 0, 0, 0]])
+    #
+    #     state = next_state
+    #     break
+    #
+    # print("Task schedule:", schedule)
